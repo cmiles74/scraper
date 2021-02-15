10b6f7ac2a947354a00af89499f56edb92a305ee
Christopher Miles <twitch@nervestaple.com>
2021-02-15 09:23:06 -0500

    Updated to include more recent JavaFX deps

--- project.clj
+++ project.clj
@@ -3,7 +3,13 @@
   :url "http://github.com/cmiles74/scraper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.openjfx/javafx-controls "15.0.1"]
+                 [org.openjfx/javafx-base "15.0.1"]
+                 [org.openjfx/javafx-graphics "15.0.1"]
+                 [org.openjfx/javafx-media "15.0.1"]
+                 [org.openjfx/javafx-web "15.0.1"]
+                 [org.openjfx/javafx-swing "15.0.1"]
+                 [org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
                  [com.taoensso/timbre "4.10.0"]
                  [cheshire "5.8.0"]]
